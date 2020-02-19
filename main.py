a5a66a877e765aa4e4234915d211ec2c12d507f5
Denys Vitali <denys@denv.it>
2020-02-19 20:58:36 +0100

    Fix Reset + Strobe

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ def demo(np, np_len):
 import machine, neopixel
 from machine import ADC, Pin
 
-np_len = 30
+np_len = 50
 np = neopixel.NeoPixel(machine.Pin(0), np_len)
 
 for i in range(np_len):
-  np[i] = (255, 0, 255)
+  np[i] = (0, 0, 0)
 
 np.write()
 
@@ -42,66 +42,53 @@ strobe_pin = Pin(12, mode=Pin.OUT, value=False)
 
 # Init sequence
 rst_pin.value(True)
-strobe_pin.value(True)
-
-utime.sleep_us(20) # 18us Minimum
-strobe_pin.value(False)
 time.sleep_us(5) # 100ns Minimum
-
 rst_pin.value(False)
-utime.sleep_us(80) # reset to strobe, minimum 72us
-# trs
-strobe_pin.value(True)
-utime.sleep_us(80) # 72us Minimum, 18us per strobe pulse width
-strobe_pin.value(False)
 
-utime.sleep_us(80) # Strobe to strobe
-
-prevOctave = [0] * 7
 octave = [0] * 7
 
 animation_duration = 5
 lastPeak = [animation_duration] * 7
 
+lowBound = 500
+
 while True:
-  prevOctave = octave
   for i in range(7):
-    strobe_pin.value(True)
-    utime.sleep_us(20) # minimum pulse width = 18us
-    octave[i] = adc.read_u16()
     strobe_pin.value(False)
-    utime.sleep_us(80) # Strobe to strobe
+    utime.sleep_us(35) # minimum pulse width = 18us
+    octave[i] = adc.read_u16()
+    strobe_pin.value(True)
+    utime.sleep_us(100) # Strobe to strobe
   
 
   # Save Octave result to LED
   # 7 bands, np_led leds
 
-  led_per_band = math.floor(np_len / 7)
+  bands = 7
+
+  led_per_band = math.floor(np_len / bands)
 
   offset = 0
-  for i in range(7):
+  for i in range(bands):
     # Each octave
 
-    if octave[i] == 0:
-      # PEAK!
-      lastPeak[i] = 0
-      print("octave " + str(i) + ": PEAK!")
-    else:
-      lastPeak[i]+= 1
-      if lastPeak[i] > animation_duration:
-        lastPeak[i] = animation_duration
-
-    threshold = (1 - lastPeak[i] / animation_duration) * led_per_band
-
+    rc_index = i * 256 // bands
+    threshold = math.floor(led_per_band * (octave[i]-lowBound)/(65535-lowBound))
+    
+    if threshold < 0:
+      threshold = 0
+    
     for j in range(led_per_band):
       if(j+1 <= threshold):
-        rc_index = (offset * 256 // np_len) + j
+        #rc_index = (offset * 256 // np_len) + j
+        #np[offset + j] = wheel(rc_index & 255)
+
         np[offset + j] = wheel(rc_index & 255)
       else:
         np[offset + j] = (0, 0, 0)
     offset = offset + led_per_band
-
+  #print(octave)
   np.write()
-  time.sleep_ms(20)
+  time.sleep_us(40)
 
 #demo(np, np_len)
